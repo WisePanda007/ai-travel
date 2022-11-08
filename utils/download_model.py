7eaa46266b0ba015e71b24543efdcafbac3c33d7
guoyanchen <guoyanchen@mafengwo.com>
2022-11-08 19:01:52 +0800

    update

--- utils/download_model.py
+++ utils/download_model.py
@@ -140,13 +140,12 @@ def downloadmodel_hf(Huggingface_Token,Path_to_HuggingFace):
     os.system("""cd /content/stable-diffusion-v1-5""")
     os.system("""git init""")
     os.system("""git lfs install --system --skip-repo""")
-    url="https://USER:{}@huggingface.co/{}".format(Huggingface_Token,Path_to_HuggingFace)
-    print(url)
     os.system(
-        """git remote add -f origin """+url)
+        """git remote add -f origin "https://USER:{}@huggingface.co/{}" """.format(Huggingface_Token,Path_to_HuggingFace))
     os.system("""git config core.sparsecheckout true""")
     os.system("""echo -e "feature_extractor\nsafety_checker\nscheduler\ntext_encoder\ntokenizer\nunet\nmodel_index.json" > .git/info/sparse-checkout""")
     os.system("""git pull origin main""")
+    print(os.getcwd())
     if os.path.exists('/content/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin'):
         os.system(
             """git clone \"https://USER:{}@huggingface.co/stabilityai/sd-vae-ft-mse\" """.format(Huggingface_Token))

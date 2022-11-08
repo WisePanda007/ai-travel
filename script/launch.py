ffa14a01705f5ad95047762417ae6ae0aba70062
guoyanchen <guoyanchen@mafengwo.com>
2022-11-08 17:21:50 +0800

    update

--- script/launch.py
+++ script/launch.py
@@ -1,15 +1,15 @@
 import os
 import sys
+sys.path.append("/content/ai-travel/")
 import demjson
 from utils.env.xformers import install_xformers
 
 
 def main(argv):
-    sys.path.append("/content/ai-travel/")
     param = demjson.decode_file(argv[0] if len(argv) >= 1 else "config/config_demo.json")
 
     #搭建环境
-    os.system("sh ai-travel/utils/env/environment.sh")
+    os.system("sh utils/env/environment.sh")
     install_xformers()
 
     #图片格式转换

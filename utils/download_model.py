0456eaa569d466c7dc766995c489fd2cf9953144
guoyanchen <guoyanchen@mafengwo.com>
2022-11-08 18:48:58 +0800

    update

--- utils/download_model.py
+++ utils/download_model.py
@@ -141,7 +141,7 @@ def downloadmodel_hf(Huggingface_Token,Path_to_HuggingFace):
     os.system("""git init""")
     os.system("""git lfs install --system --skip-repo""")
     os.system(
-        """git remote add -f origin    \"https://USER:{}@huggingface.co/{}\" """.format(aaaa,bbbbb))
+        """git remote add -f origin 'https://USER:{}@huggingface.co/{}' """.format(Huggingface_Token,Path_to_HuggingFace))
     os.system("""git config core.sparsecheckout true""")
     os.system("""echo -e "feature_extractor\nsafety_checker\nscheduler\ntext_encoder\ntokenizer\nunet\nmodel_index.json" > .git/info/sparse-checkout""")
     os.system("""git pull origin main""")

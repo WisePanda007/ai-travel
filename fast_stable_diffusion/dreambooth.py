a1480554bd1e65f75fef7f4fe8e115e100790d1b
ubuntu <ubuntu@localhost.localdomain>
2022-11-17 22:33:25 +0800

    update

--- fast_stable_diffusion/dreambooth.py
+++ fast_stable_diffusion/dreambooth.py
@@ -35,13 +35,7 @@ class DreamBooth():
                         os.system("""mkdir -p {}""".format(CLASS_DIR))
                     os.chdir(CLASS_DIR)
                     os.system("""rm -rf Women Men Mix""")
-                    os.system(
-                        """wget -q -O Womenz 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/Regularization/Women'""")
-                    os.system(
-                        """wget -q -O Menz 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/Regularization/Men'""")
-                    os.system(
-                        """wget -q -O Mixz 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/Regularization/Mix'""")
-                    os.system("""unzip Menz > /dev/null 2>&1""")
+                    os.system("""mv /content/Fast-Dreambooth/Regularization_images/* ./""") os.system("""unzip Menz > /dev/null 2>&1""")
                     os.system("""unzip Womenz > /dev/null 2>&1""")
                     os.system("""unzip Mixz > /dev/null 2>&1""")
                     # os.system("""rm -rf Menz Womenz Mixz""")

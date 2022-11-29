096781cfe6b9ef0d5aea61a68be6c0e0554d1292
ubuntu <ubuntu@localhost.localdomain>
2022-11-29 19:30:03 +0800

    自动获取ip

--- script/Logger.py
+++ script/Logger.py
@@ -1,10 +1,29 @@
 import logging
-local_ip="101.42.97.82"
+import urllib.request
+import requests
+
+
+def get_open_ip():
+    try:
+        ip = requests.get('https://checkip.amazonaws.com').text.strip()
+    except Exception as e:
+        try:
+            ip = requests.get('http://icanhazip.com').text.strip()
+        except Exception as e:
+            ip = urllib.request.urlopen(
+                'https://ident.me').read().decode('utf8').strip()
+    return ip
+
+
+local_ip = get_open_ip()
+
+
 def get_local_logger():
-    logger = logging.getLogger(local_ip) 
+    logger = logging.getLogger(local_ip)
     console_handler = logging.StreamHandler()
     logger.addHandler(console_handler)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
     logger.setLevel(logging.INFO)
     return logger
